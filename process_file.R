646ab4018abfff2393284d22519e282baf4ca073
Robert Jankowski <robjanw11@wp.pl>
2018-09-25 15:54:22 +0200

    remove file

--- process_file.R
+++
@@ -1,9 +0,0 @@
-library(tm)
-library(stringr)
-
-Sys.setlocale(category = "LC_ALL", locale = "Polish")
-read <- readDOC(engine = "antiword")
-doc <- Corpus(URISource("test.doc", encoding = "UTF-8"), readerControl = list(reader=read, language="PL"))
-doc <- content(doc[[1]])
-doc <- str_replace_all(doc, "[\r\n]" , "")
-print(doc)
